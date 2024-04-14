11beb5efeab8c00c516b6a2924415366dc97d03d
John Riley <145623701+jrile018@users.noreply.github.com>
2024-04-13 21:31:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,44 @@ def decode(encoded_password):
     return decoded_password
 
 
+'''
+#John Riley edit to the code
+   '''
+   def decode(encoded_text):
+    digit_list = []
+    decoded_text = ""
+    for i in range(len(encoded_text)):
+        digit_list.append(int(encoded_text[i]))
+    processed_list = [number - 3 for number in digit_list]
+    for i in range(len(processed_list)):
+        decoded_text = decoded_text + str(processed_list[i])
+    return decoded_text
+
+if __name__ == '__main__':
+    option = ""
+    newstring = ""  # Initialize to avoid reference before assignment error if decode is chosen first
+    while option != "3":
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        option = input("Please enter an option: ")
+        if option == "1":
+            string_encode = input("Please enter your password to encode: ")
+            newstring = encode(string_encode)
+            print("Your password has been encoded and stored!\n")
+        elif option == "2":
+            if newstring:
+                decodedstring = decode(newstring)
+                print(f"The encoded password is {newstring}, and the original password is {decodedstring}\n")
+            else:
+                print("No encoded password to decode. Please encode a password first.\n")
+        elif option == "3":
+            break
+   '''
+'''
+
 
 #edit is the main body of code with option and user input
 if __name__ == '__main__':
